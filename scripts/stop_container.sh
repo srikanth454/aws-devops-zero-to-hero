88646b7f0d3e73611bc142c0238d1857f9a87e5a
srikanth454 <61343556+srikanth454@users.noreply.github.com>
2025-05-26 12:38:41 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,14 +2,6 @@
 #!/bin/bash
 set -e
 
-# Find and stop the specific container by image name (optional)
-containerid=$(docker ps --filter ancestor=matta639/simple-python-flask-app --format "{{.ID}}")
-
-# If container exists, stop and remove it
-if [ -n "$containerid" ]; then
-    echo "Stopping and removing container $containerid"
-    docker rm -f $containerid
-else
-    echo "No running container found for the specified image."
-fi
+# Stop the running container (if any)
+echo "Hi"
 
